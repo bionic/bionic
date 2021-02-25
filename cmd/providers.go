078db581996a050a30fa73d41927bd9670d51d1a
Alexey Shekhirin <a.shekhirin@gmail.com>
2021-02-25 20:14:09 +0300

    get rid of github dependency, use plain simple map

--- cmd/providers.go
+++ cmd/providers.go
@@ -1,52 +1,39 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/bionic-dev/bionic/providers"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
-	"net/http"
 	"os"
 	"sort"
-	"strings"
 	"text/tabwriter"
 )
+
+var providerDescriptions = map[string]string{
+	"airbnb":    "https://www.airbnb.com/privacy/manage-your-data",
+	"amazon":    "https://www.amazon.com/gp/privacycentral/dsar/preview.html",
+	"apple":     "https://privacy.apple.com/ => \"Get a copy of your data\"",
+	"duolingo":  "https://drive-thru.duolingo.com/",
+	"ebay":      "https://www.sarweb.ebay.com/sar",
+	"facebook":  "https://www.facebook.com/dyi",
+	"google":    "https://takeout.google.com/",
+	"instagram": "https://www.instagram.com/download/request/",
+	"reddit":    "https://www.reddit.com/settings/data-request",
+	"snapchat":  "https://accounts.snapchat.com/accounts/downloadmydata",
+	"telegram":  "Desktop App (https://desktop.telegram.org/ only): Settings => Advanced => Export Telegram data",
+	"tiktok":    "Mobile App: Settings => Privacy => Personalization & Data",
+	"tinder":    "https://account.gotinder.com/data",
+}
 
 var providersCmd = &cobra.Command{
 	Use:   "providers",
 	Short: fmt.Sprintf("List providers available for importing using \"bionic %s\"", importCmd.Use),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		githubProviders := map[string]string{}
-
-		resp, err := http.Get("https://raw.githubusercontent.com/bionic-dev/how-to-export-personal-data/main/readme.md")
-		if err == nil {
-			scanner := bufio.NewScanner(resp.Body)
-			for scanner.Scan() {
-				line := scanner.Text()
-				name := strings.TrimPrefix(line, "### ")
-				if name == line {
-					continue
-				}
-
-				var description string
-				for description == "" && scanner.Scan() {
-					description = scanner.Text()
-				}
-				if description != "" {
-					githubProviders[strings.ToLower(name)] = description
-				}
-			}
-
-			if err := resp.Body.Close(); err != nil {
-				return err
-			}
-		}
-
 		var lines []string
 
 		for _, provider := range providers.DefaultProviders(nil) {
-			if desc, ok := githubProviders[provider.Name()]; ok {
+			if desc, ok := providerDescriptions[provider.Name()]; ok {
 				lines = append(lines, fmt.Sprintf("%s\t%s\n", provider.Name(), desc))
 			} else {
 				lines = append(lines, fmt.Sprintf("%s\t\n", provider.Name()))
