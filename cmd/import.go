f78b45fdb74e1daa2064beb6c4a643365f6db61b
Alexey Shekhirin <a.shekhirin@gmail.com>
2021-03-04 09:43:54 +0300

    refactor: cobra commands

--- cmd/import.go
+++ cmd/import.go
@@ -1,102 +1,116 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/bionic-dev/bionic/database"
 	"github.com/bionic-dev/bionic/internal/progress"
 	"github.com/bionic-dev/bionic/providers"
+	"github.com/bionic-dev/bionic/providers/provider"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 )
 
 var importCmd = &cobra.Command{
-	Use:   "import [provider] [path]",
+	Use:   "import",
 	Short: "Import data to local db",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		providerName, inputPath := args[0], args[1]
-
-		db, err := database.New(dbPath)
-		if err != nil {
-			return err
-		}
-
-		manager, err := providers.NewManager(db, providers.DefaultProviders(db))
-		if err != nil {
-			return err
-		}
-
-		if err := manager.Migrate(); err != nil {
-			return err
-		}
-
-		provider, err := manager.GetByName(providerName)
-		if err != nil {
-			return err
-		}
-
-		importFns, err := provider.ImportFns(inputPath)
-		if err != nil {
-			return err
-		}
-
-		if err := provider.BeginTx(); err != nil {
-			return err
-		}
-		defer provider.RollbackTx() //nolint:errcheck
-
-		errs, _ := errgroup.WithContext(cmd.Context())
-
-		importProgress := progress.New()
-
-		for _, importFn := range importFns {
-			name := importFn.Name()
-			importProgress.Init(name)
-		}
-
-		importProgress.Draw()
-
-		for _, importFn := range importFns {
-			name := importFn.Name()
-			fn := importFn.Call
-
-			errs.Go(func() error {
-				defer importProgress.Draw()
-
-				err := fn()
-
-				if err != nil {
-					importProgress.Error(name)
-					return err
-				}
-
-				importProgress.Success(name)
-
-				return nil
-			})
-		}
-
-		if err := errs.Wait(); err != nil {
-			return err
-		}
-
-		err = provider.DB().
-			Create(&database.Import{
-				Provider: provider.Name(),
-			}).
-			Error
-		if err != nil {
-			return err
-		}
-
-		if err := provider.CommitTx(); err != nil {
-			return err
-		}
-
-		return nil
-	},
-	Args: cobra.MinimumNArgs(2),
 }
 
 func init() {
-	rootCmd.AddCommand(importCmd)
+	for _, p := range providers.DefaultProviders(nil) {
+		providerName := p.Name()
+
+		command := &cobra.Command{
+			Use: fmt.Sprintf("%s [path]", providerName),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				inputPath := args[0]
+
+				db, err := database.New(dbPath)
+				if err != nil {
+					return err
+				}
+
+				manager, err := providers.NewManager(db, providers.DefaultProviders(db))
+				if err != nil {
+					return err
+				}
+
+				if err := manager.Migrate(); err != nil {
+					return err
+				}
+
+				p, err := manager.GetByName(providerName)
+				if err != nil {
+					return err
+				}
+
+				importFns, err := p.ImportFns(inputPath)
+				if err != nil {
+					return err
+				}
+
+				if err := p.BeginTx(); err != nil {
+					return err
+				}
+				defer p.RollbackTx() //nolint:errcheck
+
+				errs, _ := errgroup.WithContext(cmd.Context())
+
+				importProgress := progress.New()
+
+				for _, importFn := range importFns {
+					name := importFn.Name()
+					importProgress.Init(name)
+				}
+
+				importProgress.Draw()
+
+				for _, importFn := range importFns {
+					name := importFn.Name()
+					fn := importFn.Call
+
+					errs.Go(func() error {
+						defer importProgress.Draw()
+
+						err := fn()
+
+						if err != nil {
+							importProgress.Error(name)
+							return err
+						}
+
+						importProgress.Success(name)
+
+						return nil
+					})
+				}
+
+				if err := errs.Wait(); err != nil {
+					return err
+				}
+
+				err = p.DB().
+					Create(&database.Import{
+						Provider: p.Name(),
+					}).
+					Error
+				if err != nil {
+					return err
+				}
+
+				if err := p.CommitTx(); err != nil {
+					return err
+				}
+
+				return nil
+			},
+			Args: cobra.MinimumNArgs(1),
+		}
+
+		if describer, ok := p.(provider.ExportDescriber); ok {
+			command.Short = describer.ExportDescription()
+		}
+
+		importCmd.AddCommand(command)
+	}
 }
