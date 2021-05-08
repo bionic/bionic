657de2170880d51854385823fddb2c3aacc62323
Seva Zhidkov <seva@zhidkoff.com>
2021-05-08 17:31:57 +0300

    copy chrome to a tmp file

--- imports/chrome/db.go
+++ imports/chrome/db.go
@@ -3,12 +3,44 @@ package chrome
 import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 const dbRowSelectLimit = 100
 
 func (p *chrome) importDB(inputPath string) error {
-	db, err := gorm.Open(sqlite.Open(inputPath), &gorm.Config{})
+	// strip sqlite arguments
+	var filePath string
+	var arguments string
+	parts := strings.Split(inputPath, "?")
+	if len(parts) > 1 {
+		filePath = strings.Join(parts[:len(parts)-1], "?")
+		arguments = parts[len(parts)-1]
+	} else {
+		filePath = parts[0]
+	}
+
+	sourceFile, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+
+	tmpFile, err := ioutil.TempFile("", "bionic-chrome-copy.*.sqlite")
+	if err != nil {
+		return err
+	}
+
+	defer os.Remove(tmpFile.Name())
+
+	_, err = io.Copy(tmpFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	db, err := gorm.Open(sqlite.Open(tmpFile.Name()+"?"+arguments), &gorm.Config{})
 
 	if err != nil {
 		return err
